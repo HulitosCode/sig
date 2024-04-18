e423e55af5b9ab009195f802537ae6b245abff9f
HulitosCode <heltonmatimbe8@gmail.com>
2024-04-18 11:29:10 +0200

    Actualizando cookies

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,3 +249,17 @@
   new PureCounter();
 
 })()
+
+
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = 'sim'
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
